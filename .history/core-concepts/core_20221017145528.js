85ea1a8be8559999aa2cfd4c45a76e114e09bc72
khalid <dev.khalidhossain@gmail.com>
2022-10-18 01:15:47 +0600

    Practiced

---
+++ .history/core-concepts/core_20221017145528.js
@@ -0,0 +1,9 @@
+var x = 2;
+
+function foo() {
+  console.log("foo");
+}
+
+function bar() {
+  console.log("bar");
+}
---
+++ .history/core-concepts/core_20221017145639.js
@@ -0,0 +1,11 @@
+var x = 2;
+
+function foo() {
+  console.log("foo");
+}
+
+function bar() {
+  console.log("bar");
+}
+
+console.log(x);
