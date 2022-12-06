f570df7eae082a87119df45f9528a11d7adaa602
khalid <dev.khalidhossain@gmail.com>
2022-12-06 20:52:20 +0600

    Hold Shift and Check Checkboxes

--- .history/regExp/app_20221206192141.js
+++ .history/regExp/app_20221206192141.js
@@ -4,10 +4,10 @@
 // const regExp = /^([1-9]{1}[0-9]{9})$/;
 //! We are checking the country code using <areacode> "Named Capturing Group"
 const regex =
-    /(?:(?<areacode>\+\d{1,3}[ -]))?\(?(\d{3,5})\)?[ -]?(\d{3,5})[ -]?(\d{3,5})/gim;
+  /(?:(?<areacode>\+\d{1,3}[ -]))?\(?(\d{3,5})\)?[ -]?(\d{3,5})[ -]?(\d{3,5})/gim;
 
-let phoneNumber = "+91 (123) 456 7890"; //+91
-phoneNumber = "+88 017 0610 7019"; //+88
+let phoneNumber = "+91 (123) 456 7890";
+phoneNumber = "+88 017 0610 7019";
 const match = regex.exec(phoneNumber);
 // console.log(match.groups.areacode);
 console.log("Area Code: ", match.groups.areacode);
--- .history/regExp/app_20221206192351.js
+++ .history/regExp/app_20221206192351.js
@@ -6,8 +6,8 @@
 const regex =
     /(?:(?<areacode>\+\d{1,3}[ -]))?\(?(\d{3,5})\)?[ -]?(\d{3,5})[ -]?(\d{3,5})/gim;
 
-let phoneNumber = "+91 (123) 456 7890"; //+91
-phoneNumber = "+88 017 0610 7019"; //+88
+let phoneNumber = "+91 (123) 456 7890";
+phoneNumber = "+88 017 0610 7019";
 const match = regex.exec(phoneNumber);
 // console.log(match.groups.areacode);
 console.log("Area Code: ", match.groups.areacode);
